555d3b741054473ee1fe814d682f55c3c625e37a
Lobarev Vadim <96525609+LobarevVadim@users.noreply.github.com>
2022-11-24 14:06:13 +0600

    First commmit
    Just uploading of files

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,27 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Make purchases safer, because we ensure the security of your data.
+        PayPal's Buyer Protection program covers products that meet our
+        requirements if they are not delivered or do not match the description.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
